1b3ccfc5e328a7f62923a90d0631ecd5e7387d73
ANURAG-2043 <itsaworkspace2043@gmail.com>
2024-10-17 14:57:57 +0530

    dashboard added

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from routes.auth import auth_bp
+from routes.dashboard import dashboard_bp
+
 app = Flask(__name__)
+
+# Register blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == 'main':
-    app.run(debug = True)
-    
+if __name__ == '__main__':
+    app.run(debug=True)
