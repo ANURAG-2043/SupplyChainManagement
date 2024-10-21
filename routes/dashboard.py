e2d0411fbe354f88e59a1a37213fe3f9c6994ced
ANURAG-2043 <itsaworkspace2043@gmail.com>
2024-10-21 23:10:26 +0530

    stock; revenue; bloackchain & order model integrated

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+# from flask import Blueprint, render_template
 
-dashboard_bp = Blueprint('dashboard', __name__)
+# dashboard_bp = Blueprint('dashboard', __name__)
 
 # Route to render the dashboard page
-@dashboard_bp.route('/')
-def dashboard_home():
-    return render_template('dashboard.html')
+# @dashboard_bp.route('/')
+# def dashboard_home():
+#     return render_template('dashboard.html')
 
 # Route to render the orders page
 # @dashboard_bp.route('/orders')
@@ -13,21 +13,21 @@ def dashboard_home():
 #     return render_template('orders.html')
 
 # Route to render the inventory page
-@dashboard_bp.route('/inventory')
-def inventory_page():
-    return render_template('inventory.html')
+# @dashboard_bp.route('/inventory')
+# def inventory_page():
+#     return render_template('inventory.html')
 
 # Route to render the reports page
-@dashboard_bp.route('/transportation')
-def reports_page():
-    return render_template('transportation.html')
+# @dashboard_bp.route('/transportation')
+# def reports_page():
+#     return render_template('transportation.html')
 
 # Route to render the analytics page
-@dashboard_bp.route('/analytics')
-def analytics_page():
-    return render_template('analytics.html')
+# @dashboard_bp.route('/analytics')
+# def analytics_page():
+#     return render_template('analytics.html')
 
 # Route to render the visualization page
-@dashboard_bp.route('/visualization')
-def visualization_page():
-    return render_template('visualization.html')
+# @dashboard_bp.route('/visualization')
+# def visualization_page():
+#     return render_template('visualization.html')
